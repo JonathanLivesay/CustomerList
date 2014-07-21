b41a13585d97e5462ab6bccd3986edc4f62a0d68
Jonathan David Livesay <jonathan.livesay@gmail.com>
2014-07-21 11:21:25 -0500

    Committing the first part of the main project with the customer.cs file and the tests

---
+++ MVCApp3.Domain.Tests/When_Customer_Logs_In.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MVCApp3.Domain.Tests
+{
+    [TestFixture]
+    public class When_Customer_Logs_In
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
