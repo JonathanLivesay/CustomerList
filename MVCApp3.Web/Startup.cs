e79d3d963ca3eeeb2abdd7fd60038fb006cbc848
Jonathan David Livesay <jonathan.livesay@gmail.com>
2014-07-21 11:24:36 -0500

    Initial commit for the web project with the files for it

---
+++ MVCApp3.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCApp3.Web.Startup))]
+namespace MVCApp3.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
